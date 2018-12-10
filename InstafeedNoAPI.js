4fd6ae580d47a5effd7d77fe3c5079ff095f39da
soloroman <solo.roman@gmail.com>
2018-12-10 22:01:12 +0300

    Add files via upload

---
+++ InstafeedNoAPI.js
@@ -0,0 +1,25 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function getInstaProfile(username, callback){
+    httpGetAsync("https://www.instagram.com/"+username, function (response) {
+        let result = response.split("window._sharedData = ");
+        result = result[1].split(";</script>");
+        //let json = JSON.parse(result[0]);
+        let json = result[0];
+        callback(json);
+    });
+}
+
+getInstaProfile("bmw", function(json) {
+    console.log(json);
+});
+
